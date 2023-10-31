a16a5d7dd96477737ec7c0e3b9a80e236adde79e
jhaakma <jchaakma@gmail.com>
2023-10-31 20:36:04 +1300

    Clean up junk files

--- Data Files/MWSE/mods/mer/theGuarWhisperer/config/Properties.lua
+++ Data Files/MWSE/mods/mer/theGuarWhisperer/config/Properties.lua
@@ -1,7 +1,6 @@
 ---This file contains static properties that cannot be changed.
 ---@class GuarWhisperer.Config.Properties
 local Properties = {
-    MERCHANT_CONTAINER_ID = "mer_tgw_crate",
     WAITING_IDLE_CHANCES = {
         { group = "idle3", maxChance =  25 },  --sit
         { group = "idle4", maxChance = 50 },  --eat
